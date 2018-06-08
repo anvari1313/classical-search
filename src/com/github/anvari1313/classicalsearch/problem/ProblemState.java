c7b45bb8c640c1657c68f7bda3a4515c64851e77
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-08 15:27:55 +0430

    Adding action sequence to problem state

--- src/com/github/anvari1313/classicalsearch/problem/ProblemState.java
+++ src/com/github/anvari1313/classicalsearch/problem/ProblemState.java
@@ -1,6 +1,27 @@
 package com.github.anvari1313.classicalsearch.problem;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public abstract class ProblemState {
+    private List<ProblemAction> actionSequence;
+
+    public ProblemState(){
+        actionSequence = new LinkedList<>();
+    }
+
+    public ProblemState(List<ProblemAction> parentActionSequence) {
+        this();
+        this.actionSequence.addAll(parentActionSequence);
+    }
+
+    public void addActionSequence(ProblemAction action){
+        this.actionSequence.add(action);
+    }
+
+    public List<ProblemAction> getActionSequence() {
+        return actionSequence;
+    }
 
     public abstract boolean equals(ProblemState o);
 }
--- src/com/github/anvari1313/classicalsearch/problem/rescuerobot/RescueRobotProblemState.java
+++ src/com/github/anvari1313/classicalsearch/problem/rescuerobot/RescueRobotProblemState.java
@@ -1,12 +1,21 @@
 package com.github.anvari1313.classicalsearch.problem.rescuerobot;
 
+import com.github.anvari1313.classicalsearch.problem.ProblemAction;
 import com.github.anvari1313.classicalsearch.problem.ProblemState;
+
+import java.util.List;
 
 public class RescueRobotProblemState extends ProblemState {
     private int currentRow;
     private int currentCol;
 
     public RescueRobotProblemState(int currentCol, int currentRow){
+        this.currentCol = currentCol;
+        this.currentRow = currentRow;
+    }
+
+    public RescueRobotProblemState(int currentCol, int currentRow, List<ProblemAction> parentActionSequence){
+        super(parentActionSequence);
         this.currentCol = currentCol;
         this.currentRow = currentRow;
     }

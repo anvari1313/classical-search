67d24d1de2036bb17f38d5b9cb2c3750e711f0ef
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-10 12:36:54 +0430

    Adding graph search field

--- src/com/github/anvari1313/classicalsearch/search/Search.java
+++ src/com/github/anvari1313/classicalsearch/search/Search.java
@@ -7,11 +7,18 @@ import com.github.anvari1313.classicalsearch.problem.ProblemState;
 import java.util.List;
 
 public abstract class Search{
-    protected Problem<ProblemState, ProblemAction>problem;
+    protected Problem<ProblemState, ProblemAction> problem;
+
+    protected boolean graphSearch;
 
     public Search(Problem p){
         this.problem = p;
+        this.graphSearch = true;    // Enable graph search by default
     }
 
     public abstract List<ProblemAction> solve();
+
+    public void setGraphSearch(boolean graphSearch) {
+        this.graphSearch = graphSearch;
+    }
 }

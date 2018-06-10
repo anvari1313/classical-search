453ec8c38d5586b6ecbde3ccd39da810ddaa124f
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-10 11:50:48 +0430

    Implementing sliding puzzle state

--- src/com/github/anvari1313/classicalsearch/problem/slidingpuzzle/SlidingPuzzleProblemState.java
+++ src/com/github/anvari1313/classicalsearch/problem/slidingpuzzle/SlidingPuzzleProblemState.java
@@ -1,10 +1,32 @@
 package com.github.anvari1313.classicalsearch.problem.slidingpuzzle;
 
+import com.github.anvari1313.classicalsearch.problem.ProblemAction;
 import com.github.anvari1313.classicalsearch.problem.ProblemState;
 
+import java.util.List;
+
 public class SlidingPuzzleProblemState extends ProblemState {
+    private int [][] table;
+
+    private void initTable(int [][]table){
+        this.table = new int[SlidingPuzzleProblem.PUZZLE_SIZE][SlidingPuzzleProblem.PUZZLE_SIZE];
+        for (int i = 0; i < SlidingPuzzleProblem.PUZZLE_SIZE; i++) {
+            System.arraycopy(table[i], 0, this.table[i], 0, SlidingPuzzleProblem.PUZZLE_SIZE);
+        }
+    }
+
+    public SlidingPuzzleProblemState(int [][]table){
+        super();
+        initTable(table);
+    }
+
+    public SlidingPuzzleProblemState(int [][]table, List<ProblemAction> parentActionSequence){
+        super(parentActionSequence);
+        initTable(table);
+    }
+
     @Override
-    public boolean equals(ProblemState o) {
+    public boolean equalityCheck(ProblemState o) {
         return false;
     }
 }

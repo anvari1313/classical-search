b3eac35241ec564c0888a7ff2f1745a9b4c84ca0
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-10 11:30:57 +0430

    Implementing action class for sliding puzzle problem

--- src/com/github/anvari1313/classicalsearch/problem/slidingpuzzle/SlidingPuzzleProblemAction.java
+++ src/com/github/anvari1313/classicalsearch/problem/slidingpuzzle/SlidingPuzzleProblemAction.java
@@ -3,4 +3,25 @@ package com.github.anvari1313.classicalsearch.problem.slidingpuzzle;
 import com.github.anvari1313.classicalsearch.problem.ProblemAction;
 
 public class SlidingPuzzleProblemAction extends ProblemAction {
+    public enum SlidingPuzzleProblemActions {
+        R,  // Stands for move to right
+        U,  // Stands for move to up
+        L,  // Stands for move to left
+        D   // Stands for move to down
+    };
+
+    private SlidingPuzzleProblemActions action;
+
+    public SlidingPuzzleProblemAction(SlidingPuzzleProblemActions a){
+        this.action = a;
+    }
+
+    public SlidingPuzzleProblemActions getAction() {
+        return action;
+    }
+
+    @Override
+    public String toString() {
+        return action.toString();
+    }
 }

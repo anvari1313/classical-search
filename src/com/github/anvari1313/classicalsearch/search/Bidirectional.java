71530de0a9402869a22c21c0ea4700260b12c584
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-14 20:04:42 +0430

    Adding bidrectional

---
+++ src/com/github/anvari1313/classicalsearch/search/Bidirectional.java
@@ -0,0 +1,17 @@
+package com.github.anvari1313.classicalsearch.search;
+
+import com.github.anvari1313.classicalsearch.problem.Problem;
+import com.github.anvari1313.classicalsearch.problem.ProblemAction;
+
+import java.util.List;
+
+public class Bidirectional extends Search {
+    public Bidirectional(Problem p) {
+        super(p);
+    }
+
+    @Override
+    public List<ProblemAction> solve() {
+        return null;
+    }
+}

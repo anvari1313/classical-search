5e9f4a0159f28c1617e3260cc3d8214970456de9
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-14 20:05:42 +0430

    Adding AStar search class

---
+++ src/com/github/anvari1313/classicalsearch/search/AStar.java
@@ -0,0 +1,18 @@
+package com.github.anvari1313.classicalsearch.search;
+
+import com.github.anvari1313.classicalsearch.problem.Problem;
+import com.github.anvari1313.classicalsearch.problem.ProblemAction;
+
+import java.util.List;
+
+public class AStar extends Search {
+
+    public AStar(Problem p) {
+        super(p);
+    }
+
+    @Override
+    public List<ProblemAction> solve() {
+        return null;
+    }
+}
